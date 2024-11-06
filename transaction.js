8652beccf2f235bea2ca69969e15026111a6592c
naxifa <maliha1@usf.edu>
2024-11-05 19:41:31 -0500

    Create transaction module with Transaction class.

--- transaction.js
+++ transaction.js
@@ -9,19 +9,21 @@ export class Transaction {
         this.asset = getAssetById(assetId);
         this.type = type;
         this.quantity = quantity;
-        this.processTransaction();
+        this.apply();                     
     }
 
-    processTransaction() {
+    apply() {
         if (this.type === "buy") {
+            // Increase the asset quantity for a buy transaction
             this.asset.quantity += this.quantity;
         } else if (this.type === "sell") {
+            // Check if there is enough quantity for a sell transaction
             if (this.asset.quantity >= this.quantity) {
                 this.asset.quantity -= this.quantity;
             } else {
+                // Throw an error if there is not enough quantity for the sale
                 throw new Error(`Insufficient quantity for sale of ${this.asset.name}`);
             }
         }
     }
 }
-
